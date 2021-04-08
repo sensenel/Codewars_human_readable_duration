9b7f12f31519dff98f6b588341c1cfaa78a177cc
ChriGa <supersagasso@yahoo.de>
2021-04-08 20:27:21 +0200

    OOP v1 condition fehler

--- main.js
+++ main.js
@@ -12,56 +12,53 @@
  * Note that spaces are important.
  */
 
+ class timeUnits  {
+    constructor(input) {
+        this.year = 31536000,
+        this.day = 86400,
+        this.hour = 3600,
+        this.minute = 60,
+        this.seconds = 1,
+        this.input = input,
+        this.result = {}
+    }
+
+    durationMap() {
+        Object.keys(this).forEach(key => {
+            this.result[key] = Math.floor(this.input / this[key]);
+            for(let i of Object.entries(this.result)) {
+                (this.result.year > 0 && this.result.day > 365) ?
+                 this.result.day = this.result.day - 365 : 'day-error';
+                 //console.log('i: ' + i);
+                 
+                 (this.result.day > 0 && this.result.hour > 3600) ?
+                 this.result.hour = (this.result.hour - (365*24)) - 24  : 'hour-error';
+                 //console.log('i: ' + i);
+                 
+                 (this.result.hour > 0 && this.result.minute > 60) ?
+                 this.result.minute = (this.result.minute - (365*24*60)) - 60 : 'minute-error'; // kommt nicht in die Bedingung - CHECKEN!
+                 //console.log('i: ' + i);
+            }
+        });
+        delete this.result.input && delete this.result.result;
+        return this.result;
+    }
+}
 
 function formatDuration (seconds) {
 
-    const timeUnits = {
-        year: 31536000,
-        day: 86400,
-        hour: 3600,
-        minute: 60,
-        second: 1
-    }
-    let result = {}, pluralAppend = '';
-
-    if(seconds === 0 ) { return "now"}
-    else if(seconds < 0 ) {return "no valid number"}
-    else if(seconds === 1) {return `${seconds} second`}
-    else if(seconds === 3600) {return '1 hour' }
-    else {
-        Object.keys(timeUnits).forEach(key => {
-            result[key] = seconds / timeUnits[key];
-        });
-        if(seconds < timeUnits.hour) {
-                //console.log(result);
-            if(result.second % 60) {
-                pluralAppend = ` and ${result.second % 60} seconds`;
-            }
-        let resultSecond = Math.floor(result.minute);
-            return (resultSecond < 2 ) ? `${resultSecond} minute${pluralAppend}` : `${resultSecond} minutes${pluralAppend}`;
-
-        } else if(seconds < timeUnits.day) {  
-            if(result.minute % 3600) {
-                pluralAppend = ` and ${result.minute % 3600} minutes`;
-            }
-        let resultMinute = Math.floor(result.hour);
-
-            return (resultMinute < 2 ) ? `${resultMinute} hour${pluralAppend}` : `${resultMinute} minutes${pluralAppend}`;
-        }
-    }
-
-/*     for(let [key, value] of Object.entries(result)) {
-        console.log(`${key}: ${value}`);
-    } */
-
-    //return result;
 
 }
-  
+
+
+let test = new timeUnits(3600); // 1j 1d 1std 1min 1sec : 31626061 
+
+console.log(test.durationMap());
+
 
 //console.log(formatDuration(60));
 
-console.log('\n');
+/* console.log('\n');
 console.log('now\n' + formatDuration(0));//, "1 second");
 console.log('-----------------------------\n');
 console.log('1 second\n' + formatDuration(1));//, "1 second");
@@ -73,4 +70,4 @@ console.log('-----------------------------\n');
 console.log('1 hour\n'+ formatDuration(3600));//, "1 hour");
 console.log('-----------------------------\n');
 console.log('1 hour, 1 minute and 2 seconds\n' + formatDuration(3662));//, "1 hour, 1 minute and 2 seconds");
-console.log('\n');
+console.log('\n'); */
