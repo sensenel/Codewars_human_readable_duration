9d790d55db6fcbdb1491064591baad2603cd9b46
ChriGa <supersagasso@yahoo.de>
2021-04-09 17:20:18 +0200

    first working Version

--- main.js
+++ main.js
@@ -14,52 +14,43 @@
 
  class timeUnits  {
     constructor(input) {
-        this.year = 31536000,
-        this.day = 86400,
-        this.hour = 3600,
-        this.minute = 60,
-        this.seconds = 1,
-        this.input = input,
-        this.result = {}
+        this.year = Math.floor(input / 31536000),
+        this.day = Math.floor(((input % 31536000)) / 86400),
+        this.hour = Math.floor((((input % 31536000)) % 86400)/3600),
+        this.minute =  Math.floor((((input % 31536000) % 86400) % 3600) / 60),
+        this.second = Math.floor((((input % 31536000) % 86400) % 3600) % 60)
+        //this.input = input;
+    } 
+    get units() {
+        return this;
     }
-
-    durationMap() {
-        Object.keys(this).forEach(key => {
-            this.result[key] = Math.floor(this.input / this[key]);
-            for(let i of Object.entries(this.result)) {
-                (this.result.year > 0 && this.result.day > 365) ?
-                 this.result.day = this.result.day - 365 : 'day-error';
-                 //console.log('i: ' + i);
-                 
-                 (this.result.day > 0 && this.result.hour > 3600) ?
-                 this.result.hour = (this.result.hour - (365*24)) - 24  : 'hour-error';
-                 //console.log('i: ' + i);
-                 
-                 (this.result.hour > 0 && this.result.minute > 60) ?
-                 this.result.minute = (this.result.minute - (365*24*60)) - 60 : 'minute-error'; // kommt nicht in die Bedingung - CHECKEN!
-                 //console.log('i: ' + i);
-            }
-        });
-        delete this.result.input && delete this.result.result;
-        return this.result;
+    get unitMatrix() {
+        return [[... Object.values(this)]];
     }
 }
 
 function formatDuration (seconds) {
+    let result = new timeUnits(seconds), output = '', unitMatrix = result.unitMatrix;
+    
+    for(let [key, value] of Object.entries(result.units)) {
+        if (value > 0) {
+            output += value + ' ' + key + (value > 1 ? 's' : '') + ', ';
+        } 
+    }
 
+        //final format / trim and "and"
+        let lastIndex = output.lastIndexOf(', ');
+        output = output.substring(0, lastIndex);
+        
+        lastIndex = output.lastIndexOf(', ');
+        if (lastIndex > 0) {
+            output = output.substring(0, lastIndex) + ' and ' + output.substring(lastIndex + 2);
+        }
 
+    return (seconds === 0) ? 'now' : output;    
 }
 
-
-let test = new timeUnits(3600); // 1j 1d 1std 1min 1sec : 31626061 
-
-console.log(test.durationMap());
-
-
-//console.log(formatDuration(60));
-
-/* console.log('\n');
-console.log('now\n' + formatDuration(0));//, "1 second");
+console.log('\n');
 console.log('-----------------------------\n');
 console.log('1 second\n' + formatDuration(1));//, "1 second");
 console.log('-----------------------------\n');
@@ -70,4 +61,4 @@ console.log('-----------------------------\n');
 console.log('1 hour\n'+ formatDuration(3600));//, "1 hour");
 console.log('-----------------------------\n');
 console.log('1 hour, 1 minute and 2 seconds\n' + formatDuration(3662));//, "1 hour, 1 minute and 2 seconds");
-console.log('\n'); */
+console.log('\n');
